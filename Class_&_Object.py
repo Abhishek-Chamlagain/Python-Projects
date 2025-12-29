f7aec1e6a5d404ca25cd007a5c9e2a46da816b50
Python-Projects <abishekchamlagain@gmail.com>
2025-12-29 20:33:03 +0545

    Class and object in python

---
+++ Class_&_Object.py
@@ -0,0 +1,34 @@
+"""To map with real world scenarios, we start using object in code. 
+This is also called object oriented programming."""
+
+'Class is a blueprint for creating object'
+
+'Creating class'
+# class Student:
+#     name = "Abhishek Chamlagain"
+
+'Creating object'
+# S1 = Student()
+# print(S1.name)
+
+'Another Example'
+# class Car:
+#     color = "Black"
+#     brand = "BMW"
+
+# car1 = Car()
+# print(car1.color)
+# print(car1.brand)
+
+"Constructor"
+'''All classes have a function called __init__(), 
+which is always executed when the object is being initiated.'''
+
+"Creating class"
+# class Student:
+#     def __init__(Self, fullname):
+#         Self.name = fullname
+
+"Creating Object"
+# S1=Student("Abhishek")
+# print(S1.name)
